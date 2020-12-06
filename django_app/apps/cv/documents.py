2e141fbd8aeb6fe6b70cc7846b0b5e45137ddb36
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2020-12-06 09:05:18 +0100

    adding Elasticsearch ResumeDocument

---
+++ django_app/apps/cv/documents.py
@@ -0,0 +1,39 @@
+from django_elasticsearch_dsl import Document, fields
+from django_elasticsearch_dsl.registries import registry
+from .models import Resume
+
+
+@registry.register_document
+class ResumeDocument(Document):
+    # degree = [str]
+    # designition = [str]
+    # email = str
+    # phone = str
+    # name = str
+    # total_exp = int
+    # university = [str]
+    # skills = [str]
+    full_name = fields.TextField(attr="full_name")
+    text = fields.TextField(attr="text")
+
+    class Index:
+        name = 'resumes'  # Name of the Elasticsearch index
+        # See Elasticsearch Indices API reference for available settings
+        # settings = {'number_of_shards': 1, 'number_of_replicas': 0}
+
+    class Django:
+        model = Resume  # The model associated with this Document
+        # The fields of the model you want to be indexed in Elasticsearch
+        fields = [
+            'first_name',
+            'last_name',
+        ]
+        # Ignore auto updating of Elasticsearch when a model is saved or deleted:
+        # ignore_signals = True
+
+        # Don't perform an index refresh after every update (overrides global setting):
+        # auto_refresh = False
+
+        # Paginate the django queryset used to populate the index with the specified size
+        # (by default it uses the database driver's default setting)
+        # queryset_pagination = 5000
