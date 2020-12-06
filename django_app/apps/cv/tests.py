8767c05746bcf49e6f3afc0c8ccde649930a3526
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2020-12-06 09:38:09 +0100

    Adding tests

--- django_app/apps/cv/tests.py
+++ django_app/apps/cv/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+"""
+tests for CV parsing: PDF, Word and Images.
+run:
+./manage.py test apps.cv.tests.TestUtils
+"""
+import os
+from django.conf import settings
+from unittest import TestCase
+from .utils import extract_text_from_file
 
-# Create your tests here.
+absolute_path = os.path.join(settings.BASE_DIR, 'media', 'CVs')
+
+
+class TestUtils(TestCase):
+
+    def test_pdf_parser(self):
+        text = extract_text_from_file(os.path.join(absolute_path, 'cv6.pdf'))
+        self.assertTrue(text)
+        self.assertIsInstance(text, str)
+
+    def test_word_parser(self):
+        text = extract_text_from_file(os.path.join(absolute_path, 'cv11.docx'))
+        self.assertTrue(text)
+        self.assertIsInstance(text, str)
+
+    def test_png_parser(self):
+        text = extract_text_from_file(os.path.join(absolute_path, 'cv5.png'))
+        self.assertTrue(text)
+        self.assertIsInstance(text, str)
+
+    def test_jpg_parser(self):
+        text = extract_text_from_file(os.path.join(absolute_path, 'cv4.jpg'))
+        self.assertTrue(text)
+        self.assertIsInstance(text, str)
